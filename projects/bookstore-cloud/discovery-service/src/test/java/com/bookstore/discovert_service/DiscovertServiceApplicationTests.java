ecf5645735726f1218fdd7772632b0ecb30a3ba4
Andre Marcos <andre92marcos@gmail.com>
2025-07-17 09:54:43 +0100

    Initial commit for thir project

---
+++ projects/bookstore-cloud/discovery-service/src/test/java/com/bookstore/discovert_service/DiscovertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.discovert_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscovertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
